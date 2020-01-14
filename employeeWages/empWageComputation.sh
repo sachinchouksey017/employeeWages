1bd91d091ef739434135f2208a9b525d63e76e76
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-01-14 11:57:09 +0530

    ADDED-check emp is present or absent completed

--- employeeWages/empWageComputation.sh
+++ employeeWages/empWageComputation.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
+
 echo "Welcome to Employee Wage Computation program"
+
+#UC-1
+function isEmpPresent(){
+   local isPresent=1
+   local randomCheck=$(( RANDOM%2 ))
+   if [ $randomCheck -eq $isPresent ]
+   then
+      echo "Employee is Present "
+   else
+      echo "Employee is Absent "
+   fi
+}
+isEmpPresent
