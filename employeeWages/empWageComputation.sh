e1488569d5e3636d0c05de38842e32f947ea871c
sachinchouksey017 <sachinchouksey017@gmail.com>
2020-01-14 13:03:16 +0530

    Added:-also calculated parttime employee wages

--- employeeWages/empWageComputation.sh
+++ employeeWages/empWageComputation.sh
@@ -30,3 +30,22 @@ function dailyEmpWages(){
 
 }
 dailyEmpWages
+
+#UC-3
+function partTimeEmpWages(){
+   local isPartTime=1;
+   local isFullTime=2;
+   local empRatePerHrs=20;
+   local randomCheck=$(( RANDOM%3 ))
+   if [ $randomCheck -eq $isFullTime ]
+   then
+      empHrs=8;
+   elif [ $randomCheck -eq $isPartTime ]
+   then
+      empHrs=4;
+   else
+      empHrs=0;
+   fi
+      salary=$(( empRatePerHrs*empHrs ));
+}
+partTimeEmpWages
